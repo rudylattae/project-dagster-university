f7a558e8f3559aff1e308d3fd6e9001dc728701b
Rudy Lattae <rudylattae@gmail.com>
2026-01-29 06:36:15 +0000

    Add asset def to load raw taxi_trips into duckdb table.

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -1,6 +1,10 @@
+import os
+import duckdb
 import requests
 import dagster as dg
 from dagster_essentials.defs.assets import constants
+from dagster._utils.backoff import backoff
+
 
 @dg.asset
 def taxi_trips_file() -> None:
@@ -27,3 +31,36 @@ def taxi_zones_file() -> None:
 
     with open(constants.TAXI_ZONES_FILE_PATH, 'wb') as f:
         f.write(zones_data.content)
+
+
+@dg.asset(
+    deps=[taxi_trips_file],
+)
+def taxi_trips() -> None:
+    """A DuckDB table containing the rawtaxi trips data."""
+    month_to_fetch = '2023-03'
+    raw_file_path = constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)
+    query = f"""
+        CREATE OR REPLACE TABLE taxi_trips AS (
+            SELECT VendorID as vendor_id,
+                PULocationID as pickup_zone_id,
+                DOLocationID as dropoff_zone_id,
+                RatecodeID as rate_code_id,
+                payment_type as payment_type,
+                tpep_dropoff_datetime as dropoff_datetime,
+                tpep_pickup_datetime as pickup_datetime,
+                trip_distance as trip_distance,
+                passenger_count as passenger_count,
+                total_amount as total_amount
+        FROM '{raw_file_path}');
+    """
+
+    dg.get_dagster_logger().info(f'Loading raw data from {raw_file_path} into DuckDB table taxi_trips')
+
+    conn = backoff(
+        fn=duckdb.connect,
+        retry_on=(RuntimeError, duckdb.IOException),
+        kwargs={'database': os.getenv('DUCKDB_DATABASE')},
+        max_retries=10,
+    )
+    conn.execute(query)

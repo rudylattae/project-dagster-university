9c0500e890fa67567f1db5b23b9cb3e448153fe5
Rudy Lattae <rudylattae@gmail.com>
2026-01-29 06:13:52 +0000

    Add asset def for taxi zones csv file

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -14,3 +14,16 @@ def taxi_trips_file() -> None:
 
     with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), 'wb') as f:
         f.write(raw_trips.content)
+
+
+@dg.asset
+def taxi_zones_file() -> None:
+    """CSV of the distinct taxi zones in NYC. Sourced from NYC Open Data."""
+    zones_data_url = 'https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv'
+    
+    dg.get_dagster_logger().info(f'Fetching taxi zones data from {zones_data_url}')
+    
+    zones_data = requests.get(zones_data_url)
+
+    with open(constants.TAXI_ZONES_FILE_PATH, 'wb') as f:
+        f.write(zones_data.content)
